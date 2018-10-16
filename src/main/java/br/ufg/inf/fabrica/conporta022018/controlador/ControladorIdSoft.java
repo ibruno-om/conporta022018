ad0b06770845e88b92b52ec7407cb3182cc5bc60
BeatrizN <beatrizncsilveira@gmail.com>
2018-10-16 17:59:16 -0300

    Criando classes para testar o controlador de IdSoft

---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorIdSoft.java
@@ -0,0 +1,12 @@
+package br.ufg.inf.fabrica.conporta022018.controlador;
+
+/**
+ * Created by aluno on 16/10/18.
+ */
+public class ControladorIdSoft {
+
+    public void idSoft(String cpf, String siglaUndId, int anoId, int seqId) {
+
+    }
+
+}
