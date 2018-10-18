ce124604d71696e0963ba3325d86c708df66036c
João Lucas <joaolucaspachecoab@gmail.com>
2018-10-18 09:58:14 -0300

    Pastas e arquivos renomeados.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorExpiraPorta.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorExpiraPorta.java
@@ -6,9 +6,9 @@
 
 package br.ufg.inf.fabrica.conporta022018.controlador;
 
-public class ControladorRegCiencDesig {
+public class ControladorExpiraPorta {
 
-    public void regCiencDesig(String cpf, String siglaUndId, int anoId, int seqId) {
+    public void expiraPorta(String dataExpira) {
 
     }
 
