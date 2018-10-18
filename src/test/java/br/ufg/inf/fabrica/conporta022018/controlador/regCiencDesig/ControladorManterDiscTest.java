24021a5a5deb0fa922e17ced2fc7f777562de9b9
Edionay Aguiar <edionay@gmail.com>
2018-10-18 17:37:45 -0300

    Classe de teste renomeada para ControladorManterDiscTest.

--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/regCiencDesig/ControladorManterDiscTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/regCiencDesig/ControladorManterDiscTest.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class ControladorRegCiencDesigTest {
+public class ControladorManterDiscTest {
 
     private static ControladorDisc controladorDisc;
 
@@ -29,7 +29,7 @@ public class ControladorRegCiencDesigTest {
     @BeforeClass
     public static void casoTestPepararCenario() throws IOException {
 
-        String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/regCiencDesig/RegCienDesigDadosTest.csv";
+        String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/manterDisc/ManterDiscTest.csv";
         String REGRA = ";";
         List<String> dadosSoftware = new ArrayList<>();
         Extrator extrator = new ExtratorCSV();
@@ -45,32 +45,17 @@ public class ControladorRegCiencDesigTest {
             linha = dadosSoftware.get(index);
 
             //Definir as tabelas que serão populadas no Banco de Dados.
-            if (linha.equals("pessoa") || linha.equals("portaria") || linha.equals("undAdm") || linha.equals("designado")) {
+            if (linha.equals("discente")) {
                 tabelaAtual = linha;
                 index++;
                 continue;
             }
 
             switch (tabelaAtual) {
-                case "pessoa" :
+                case "discente" :
                     extrator.setTexto(linha);
                     dados = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela pessoa no Banco de Dados.
-                    break;
-                case "portaria" :
-                    extrator.setTexto(linha);
-                    dados = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela portaria no Banco de Dados.
-                    break;
-                case "undAdm" :
-                    extrator.setTexto(linha);
-                    dados = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela Unidade Administrativa no Banco de Dados.
-                    break;
-                case "designado" :
-                    extrator.setTexto(linha);
-                    dados = extrator.getResultado(REGRA);
-                    //Aqui colocar os comandos para popular a tabela designados no Banco de dados.
+                    //Aqui colocar os comandos para popular a tabela discente no Banco de Dados.
                     break;
             }
         }
@@ -96,7 +81,7 @@ public class ControladorRegCiencDesigTest {
     public void casoTestDadosValidos() throws IOException {
 
         //Grupo de teste DadosValidos, exemplo:
-        controladorDisc.regCiencDesig("123.456.789-12", "INF", 2018, 0001);
+//        controladorDisc.regCiencDesig("123.456.789-12", "INF", 2018, 0001);
 
     }
 
@@ -104,7 +89,7 @@ public class ControladorRegCiencDesigTest {
     public void casoTestDadosExcecoes() throws IOException {
 
         //Grupo de teste DadosExcecoes, exemplo:
-        controladorDisc.regCiencDesig("123.456.789-12", "FACE", 2018, 0001);
+//        controladorDisc.regCiencDesig("123.456.789-12", "FACE", 2018, 0001);
         //O cenario acima testa a primeira exceção do caso de uso a unidade acadêmica não é localizada.
     }
 
@@ -115,10 +100,10 @@ public class ControladorRegCiencDesigTest {
         // irá fica as suas pós-condições. Exemplo:
 
         //Busca a data atual.
-        Date hoje = new Date();
-        SimpleDateFormat df;
-        df = new SimpleDateFormat("dd/MM/yyyy");
-        String dataHoje = df.format(hoje);
+//        Date hoje = new Date();
+//        SimpleDateFormat df;
+//        df = new SimpleDateFormat("dd/MM/yyyy");
+//        String dataHoje = df.format(hoje);
 
         //pega a data que foi armazenada no banco de dados e verifica com a data de execução do teste, ou seja,
         // a data de hoje.
