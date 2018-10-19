3f0d5af1b337c5a09f384014db5a2f59822bb77b
Josneyo <jdavidcastan@hotmail.com>
2018-10-18 22:48:49 -0300

    Add ControladorEncPortTest & copied CSV Utils
    Fiquei com preguiça de fazer o merge entre os Branchs então copiei os utilitários do CSV e iniciei os testes
    
    Co-Authored-By: Antonio Silva <antoni-s@outlook.com>
    Co-Authored-By: Keslley L. <keslleylima@users.noreply.github.com>

---
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorEncPortTest.java
@@ -0,0 +1,16 @@
+package br.ufg.inf.fabrica.conporta022018.controlador;
+
+import br.ufg.inf.fabrica.conporta022018.util.Extrator;
+import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
+import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class ControladorEncPortTest {
+
+    private static ControladorEncPort controladorEncPort;
+
+    @Test
+    public void encPortariaCiencia() {
+    }
+}
