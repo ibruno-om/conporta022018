65192a98431c18ac2f4e7094299f08bd702db301
Ana Luísa Burjack <analuisaburjack@gmail.com>
2018-10-18 21:16:48 -0300

    Add files via upload

---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorGraFiltros.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2018.
+ * Fabrica de Software INF
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package br.ufg.inf.fabrica.conporta022018.controlador;
+
+import java.util.Date;
+
+public class ControladorGraFiltros {
+
+    public void getUndAdm(){ 
+    }
+    
+    public void setFiltro(){ 
+    }
+    
+    public void getDados(){ 
+    }
+    
+    public void setOrdenacao(){ 
+    }
+    
+
+}
