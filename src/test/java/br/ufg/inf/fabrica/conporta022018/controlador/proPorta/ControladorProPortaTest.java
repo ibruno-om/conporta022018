d1ef69f91d71b5f1ba3ec5e253c99d666bfa6883
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2018-10-19 20:14:41 -0300

    preparando cenário de testes e aguardando conclusão de integração de diagramas

--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/proPorta/ControladorProPortaTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/proPorta/ControladorProPortaTest.java
@@ -1,4 +1,49 @@
 package br.ufg.inf.fabrica.conporta022018.controlador.proPorta;
 
+import br.ufg.inf.fabrica.conporta022018.controlador.ControladorProPorta;
+import br.ufg.inf.fabrica.conporta022018.util.Extrator;
+import br.ufg.inf.fabrica.conporta022018.util.LerArquivo;
+import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
+import org.junit.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ControladorProPortaTest{
+    private static ControladorProPorta controladorProPorta;
+
+    @BeforeClass
+    public static void casoTestPrepararCenario() throws IOException{
+        final String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/regCiencDesig/ProPortaTest.csv";
+        final String REGRA = ";";
+        List<String> dadosSoftware = new ArrayList<>();
+        Extrator extrator = new ExtratorCSV();
+        LerArquivo lerArquivo = new LerArquivo();
+        String tabelaAtual = " ";
+        String dados[];
+        String linha;
+
+        dadosSoftware = lerArquivo.lerArquivo(CAMINHO_CSV);
+
+        for (int index = 0; index < dadosSoftware.size(); index++) {
+
+            linha = dadosSoftware.get(index);
+
+            if (){
+                tabelaAtual = linha;
+                index++;
+                continue;
+            }
+
+            switch (tabelaAtual){
+
+            }
+        }
+    }
+
+
+}
+
 
 
