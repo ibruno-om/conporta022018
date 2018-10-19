a708d40281e81c039dafcc7f20574f24c49762f9
Josneyo <jdavidcastan@hotmail.com>
2018-10-18 22:05:54 -0300

    Add ControladorEncPort
    @keslleylima

---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorEncPort.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018.
+ * Fabrica de Software INF
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package br.ufg.inf.fabrica.conporta022018.controlador;
+
+public class ControladorEncPort {
+
+    public boolean encPortariaCiencia(String IdPortaria){
+        return true;
+    }
+}
