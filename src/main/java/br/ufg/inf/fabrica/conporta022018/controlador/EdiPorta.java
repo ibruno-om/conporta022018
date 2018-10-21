3be2e86b425804928a1ddbd32a9834cfed1b07fb
Victor <victor.stillo@gmail.com>
2018-10-21 19:36:55 -0300

    Criando testes

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/EdiPorta.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/EdiPorta.java
@@ -1,4 +1,7 @@
 package br.ufg.inf.fabrica.conporta022018.controlador;
 
 public class EdiPorta {
+    public void updatePorta(){
+
+    }
 }
--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/ediPorta/EdiPortaTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/ediPorta/EdiPortaTest.java
@@ -1,19 +1,111 @@
+package br.ufg.inf.fabrica.conporta022018.controlador.ediPorta;
+
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import java.util.ArrayList;
+import java.io.IOException;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 @RunWith(Arquillian.class)
 public class EdiPortaTest {
-    @Deployment
-    public static JavaArchive createDeployment() {
-        return ShrinkWrap.create(JavaArchive.class)
-                .addClass(br.ufg.inf.fabrica.conporta022018.controlador.EdiPorta.class)
-                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+
+    @BeforeClass
+    @BeforeClass
+    public static void casoTestPepararCenario() throws IOException {
+
+        String CAMINHO_CSV = "src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/idSoft/IdSoftTest.csv";
+        String REGRA = ";";
+        List<String> dadosSoftware = new ArrayList<>();
+        Extrator extrator = new ExtratorCSV();
+        LerArquivo lerArquivo = new LerArquivo();
+        String tabelaAtual = " ";
+        String dados[];
+        String linha;
+        //Criar as instâncias de todos os objetos DAO's necessários para preparar o cenario.
+
+        dadosSoftware = lerArquivo.lerArquivo(CAMINHO_CSV);
+
+        for (int index = 0; index < dadosSoftware.size(); index++) {
+            linha = dadosSoftware.get(index);
+
+            //Definir as tabelas que serão populadas no Banco de Dados.
+            if (linha.equals("pessoa") || linha.equals("portaria") || linha.equals("undAdm") || linha.equals("designado")) {
+                tabelaAtual = linha;
+                index++;
+                continue;
+            }
+
+            switch (tabelaAtual) {
+                case "pessoa" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    //Aqui colocar os comandos para popular a tabela pessoa no Banco de Dados.
+                    break;
+                case "portaria" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    //Aqui colocar os comandos para popular a tabela portaria no Banco de Dados.
+                    break;
+                case "undAdm" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    //Aqui colocar os comandos para popular a tabela Unidade Administrativa no Banco de Dados.
+                    break;
+                case "designado" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    //Aqui colocar os comandos para popular a tabela designados no Banco de dados.
+                    break;
+            }
+        }
+    }
+
+    @Test
+    public void ediPortaParametrosValidosTest() {
+
+    }
+
+    @Test
+    public void ediPortaUsuarioNaoAdministradorTest() {
+
+    }
+
+    @Test
+    public  void ediPortaStatusNaoPropostaTest() {
+
+    }
+
+    @Test
+    public void ediPortaDadosObrigatoriosNaoPreenchidosTest() {
+
+    }
+
+    @Test
+    public  void ediPortaReferenciaInvalidaTest() {
+
+    }
+
+    @Test
+    public void ediPortaReferenciaValidaTest() {
+
+    }
+
+    @Test
+    public void ediPortaDesignadosValidosTest() {
+
+    }
+
+    @Test
+    public void ediPortaDesignadosInvalidosTest() {
+
     }
 
 }
