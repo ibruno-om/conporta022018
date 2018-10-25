30200e8d672e5b655cd892ecf6be05dd634fbede
Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
2018-10-25 17:12:55 -0300

    Signed-off-by: Rodrigo Aguiar <rodrigoaguiar0@gmail.com>
    Inserção de teste ProPorta
    
    Foi inserido testes de validaçaõ dos itens.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorProPorta.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorProPorta.java
@@ -1,5 +1,10 @@
 package br.ufg.inf.fabrica.conporta022018.controlador;
 
-    public class ControladorProPorta{
+import java.util.Date;
 
+public class ControladorProPorta{
+
+        public void ProPorta(String assunto, Date dtIniVig, Date dtFimVig, int horasDesig, String resumo, String textoCompleto){
+
+        }
     }
--- src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/proPorta/ControladorProPortaTest.java
+++ src/test/java/br/ufg/inf/fabrica/conporta022018/controlador/proPorta/ControladorProPortaTest.java
@@ -7,7 +7,10 @@ import br.ufg.inf.fabrica.conporta022018.util.csv.ExtratorCSV;
 import org.junit.*;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ControladorProPortaTest{
@@ -30,16 +33,56 @@ public class ControladorProPortaTest{
 
             linha = dadosSoftware.get(index);
 
-            if (){
+            if (linha.equals("pessoa")||linha.equals("portaria")||linha.equals("undAdm")||linha.equals("designado")){
                 tabelaAtual = linha;
                 index++;
                 continue;
             }
 
             switch (tabelaAtual){
-
+                case "pessoa" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    break;
+                case "portaria" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    break;
+                case "undAdm" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    break;
+                case "designado" :
+                    extrator.setTexto(linha);
+                    dados = extrator.getResultado(REGRA);
+                    break;
             }
         }
+    }
+
+    @Before
+    public void casoTestPrepararExecucao(){
+        controladorProPorta = new ControladorProPorta();
+    }
+
+    @Test
+    public void casoTestDadosValidos()throws ParseException {
+        DateFormat f = DateFormat.getDateInstance();
+        Date dtIniVig = f.parse("12/01/2010");
+        Date dtFimVig = f.parse("30/10/2019");
+        controladorProPorta.ProPorta("Controle de estagio", dtIniVig, dtFimVig, 8, "" +
+                "Portaria que cuida dos estagiarios na UFG", "Portaria com a responsabilidade de cuidar" +
+                "dos estagiarios e manter o funcionamento dos processos de estagio");
+    }
+
+    @Test
+    public void casoTestDadosInvalidos () throws ParseException{
+        DateFormat f = DateFormat.getDateInstance();
+        Date dtIniVig = f.parse("12/05/2019");
+        Date dtFimVig = f.parse("30/10/2019");
+        controladorProPorta.ProPorta("Controle de estagio", dtIniVig, dtFimVig, 28, "" +
+                "Portaria que cuida dos estagiarios na UFG", "Portaria com a responsabilidade de cuidar" +
+                "dos estagiarios e manter o funcionamento dos processos de estagio");
     }
 
 
