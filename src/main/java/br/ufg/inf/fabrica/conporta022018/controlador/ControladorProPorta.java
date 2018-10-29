ddf9d0d29d81db471deb49c76dec38bd95c615f9
Samuel Costa <samu.rcosta@gmail.com>
2018-10-29 02:24:06 -0300

    Inicialização do controlador

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorProPorta.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorProPorta.java
@@ -4,7 +4,31 @@ import java.util.Date;
 
 public class ControladorProPorta{
 
-        public void ProPorta(String assunto, Date dtIniVig, Date dtFimVig, int horasDesig, String resumo, String textoCompleto){
-
-        }
+    public void gerarFormulario(){
+        // Renderiza a view
     }
+
+    public void designarPessoas(){
+
+    }
+
+    public void referenciarPortarias(){
+
+    }
+
+    public void salvar(){
+
+    }
+
+    public void validarInformacoes(){
+
+    }
+
+    public void gerarBlob(){
+
+    }
+
+    public void ProPorta(String assunto, Date dtIniVig, Date dtFimVig, int horasDesig, String resumo, String textoCompleto){
+
+    }
+}
