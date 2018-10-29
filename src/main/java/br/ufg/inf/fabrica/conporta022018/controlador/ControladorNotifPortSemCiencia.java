7acea1235473ed7ace6e86079d7e8446f5c67429
João Lucas <joaolucaspachecoab@gmail.com>
2018-10-29 11:08:12 -0300

    Método atualizadoo.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorNotifPortSemCiencia.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/controlador/ControladorNotifPortSemCiencia.java
@@ -9,6 +9,8 @@ package br.ufg.inf.fabrica.conporta022018.controlador;
 public class ControladorNorifPortSemCiencia {
 
     public void notifPortSemCiencia() {
+        
+        pesquisaDesignadosSemCiencia();
 
     }
 
