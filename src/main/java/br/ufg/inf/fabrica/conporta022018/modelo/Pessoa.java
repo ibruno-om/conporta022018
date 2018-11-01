e52a916d0e0974d81f87076e49de715b60f8ba69
Edionay Aguiar <edionay@gmail.com>
2018-11-01 18:01:25 -0300

    Setters de Pessoa criados.

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Pessoa.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Pessoa.java
@@ -5,6 +5,7 @@ import br.ufg.inf.fabrica.conporta022018.persistencia.PessoaDAO;
 import java.util.List;
 
 public class Pessoa {
+
     private String nomePes;
 
     private Integer cpfPes;
@@ -20,5 +21,31 @@ public class Pessoa {
     private Designado designado;
 
     private PessoaDAO pessoaDAO;
+
+    public void setNomePes(String nomePes) {
+        this.nomePes = nomePes;
+    }
+
+    public void setCpfPes(Integer cpfPes) {
+        this.cpfPes = cpfPes;
+    }
+
+    public void setEmailPes(String emailPes) {
+        this.emailPes = emailPes;
+    }
+
+    public void setSenhaUsu(String senhaUsu) {
+        this.senhaUsu = senhaUsu;
+    }
+
+    public void setEhUsuAtivo(boolean ehUsuAtivo) {
+        this.ehUsuAtivo = ehUsuAtivo;
+    }
+
+    public void setUltimoLogin(long ultimoLogin) {
+        this.ultimoLogin = ultimoLogin;
+    }
+
+
 
 }
