c22591ba569097eebbcb42a42151663a71c9a292
Natália Marufuji <nataliamarufuji@gmail.com>
2018-11-09 20:24:12 -0200

    Adição dos relacionamentos com classe Portaria e UndAdm

--- src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Recebedora.java
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/modelo/Recebedora.java
@@ -3,12 +3,30 @@ package br.ufg.inf.fabrica.conporta022018.modelo;
 public class Recebedora {
      
     private Date dtCienciaReeb;
+    private Portaria portariaRecebida;
+    private UndAdm unidadeRecebedora;
 
-    public void setDtCienciaReeb(dtCienciaReeb : Date){
+    public void setDtCienciaReeb(Date dtCienciaReeb){
         this.dtCienciaReeb = dtCienciaReeb;
     }  
 
     public Date getDtCienciaReeb(){
         return this.dtCienciaReeb;
+    }
+
+    public void setPortariaRecebida(Portaria portariaRecebida){
+        this.portariaRecebida = portariaRecebida;
+    }  
+
+    public Date getPortariaRecebida(){
+        return this.portariaRecebida;
+    }
+
+    public void setUnidadeRecebedora(UndAdm unidadeRecebedora){
+        this.unidadeRecebedora = unidadeRecebedora;
+    }  
+
+    public Date getUnidadeRecebedora(){
+        return this.unidadeRecebedora;
     }
 }
