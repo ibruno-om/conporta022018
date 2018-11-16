dafc0144f3a4ed9077d98628fabd3606af4ba0fe
joaopsalgado <salgadojoaopedro@hotmail.com>
2018-11-15 23:14:53 -0200

    Persistencia Gestao foi complementado

---
+++ src/main/java/br/ufg/inf/fabrica/conporta022018/persistencia/GestaoDAO.java
@@ -0,0 +1,9 @@
+package br.ufg.inf.fabrica.conporta022018.persistencia;
+
+import br.ufg.inf.fabrica.conporta022018.modelo.Gestao;
+
+public class GestaoDAO extends GenericoDAO {
+
+    private Gestao gestao;
+
+}
